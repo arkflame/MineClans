1a5dd017ec097fcd664c3c65ddd566009e2f538c
linsaftw <linsaftw@users.noreply.github.com>
2024-05-16 13:58:14 -0300

    factions command create/disband test

--- src/main/java/com/arkflame/mineclans/commands/FactionsCommand.java
+++ src/main/java/com/arkflame/mineclans/commands/FactionsCommand.java
@@ -1,10 +1,13 @@
 package com.arkflame.mineclans.commands;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-import com.arkflame.mineclans.MineClans;
 import com.arkflame.modernlib.commands.ModernArguments;
 import com.arkflame.modernlib.commands.ModernCommand;
+import com.arkflame.mineclans.MineClans;
+import com.arkflame.mineclans.models.Faction;
+import com.arkflame.mineclans.models.FactionPlayer;
 
 public class FactionsCommand extends ModernCommand {
     public FactionsCommand() {
@@ -13,23 +16,69 @@ public class FactionsCommand extends ModernCommand {
 
     @Override
     public void onCommand(CommandSender sender, ModernArguments args) {
-        String label = args.getLabel();
-        String arg1 = args.getText(0);
-        int arg2 = args.getNumber(1);
-
-        if (args.hasArg(0)) {
-            sender.sendMessage("You wrote text: " + arg1);
-        } 
-
-        if (args.hasArg(1)) {
-            sender.sendMessage("You wrote a number: " + arg2);
-        } 
-        
-        if (!args.hasArg()) {
-            sender.sendMessage("You wrote no arguments");
-            sender.sendMessage("Usage: /" + label + " [text] [number]");
+        if (!args.hasArg(0)) {
+            sender.sendMessage(MineClans.getInstance().getMsg().getText("factions.usage"));
+            return;
         }
 
-        sender.sendMessage(MineClans.getInstance().getMsg().getText("messages.from-command"));
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Not from console.");
+            return;
+        }
+
+        MineClans.getInstance().runAsync(() -> {
+            Player player = (Player) sender;
+            String subcommand = args.getText(0);
+            switch (subcommand.toLowerCase()) {
+                case "create":
+                    createFaction(player, args);
+                    break;
+                case "disband":
+                    disbandFaction(player, args);
+                    break;
+                default:
+                    sender.sendMessage(MineClans.getInstance().getMsg().getText("factions.usage"));
+            }
+        });
+    }
+
+    private void createFaction(Player player, ModernArguments args) {
+        String factionName = args.getText(1);
+        if (factionName == null) {
+            player.sendMessage(MineClans.getInstance().getMsg().getText("factions.create.usage"));
+            return;
+        }
+        FactionPlayer factionPlayer = MineClans.getInstance().getFactionPlayerManager().getOrLoad(player.getUniqueId());
+        Faction faction = factionPlayer.getFaction();
+        if (faction != null) {
+            player.sendMessage("You already have a faction. Disband it or leave first.");
+            return;
+        }
+
+        faction = MineClans.getInstance().getFactionManager().createFaction(player, factionName);
+        MineClans.getInstance().getFactionPlayerManager().updateFaction(factionPlayer.getPlayerId(), faction);
+        player.sendMessage("Created faction.");
+    }
+
+    private void disbandFaction(Player player, ModernArguments args) {
+        String factionName = args.getText(1);
+        if (factionName == null) {
+            player.sendMessage(MineClans.getInstance().getMsg().getText("factions.disband.usage"));
+            return;
+        }
+
+        FactionPlayer factionPlayer = MineClans.getInstance().getFactionPlayerManager().getOrLoad(player.getUniqueId());
+        Faction faction = factionPlayer.getFaction();
+        if (faction == null) {
+            player.sendMessage("You have no faction.");
+            return;
+        }
+        if (faction.getOwner() != factionPlayer.getPlayerId()) {
+            player.sendMessage("You are not the owner.");
+            return;
+        }
+        MineClans.getInstance().getFactionManager().disbandFaction(factionName);
+        MineClans.getInstance().getFactionPlayerManager().updateFaction(factionPlayer.getPlayerId(), null);
+        player.sendMessage("Disbanded faction.");
     }
 }
