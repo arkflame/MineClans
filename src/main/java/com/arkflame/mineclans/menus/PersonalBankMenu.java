11e32b731bd738ae0dc56587f7ea997b46d3c136
linsaftw <linsaftw@users.noreply.github.com>
2024-09-01 00:08:59 -0300

    finish up bank system

--- src/main/java/com/arkflame/mineclans/menus/PersonalBankMenu.java
+++ src/main/java/com/arkflame/mineclans/menus/PersonalBankMenu.java
@@ -10,35 +10,34 @@ import com.arkflame.mineclans.models.Faction;
 import com.arkflame.mineclans.models.FactionPlayer;
 import com.arkflame.mineclans.modernlib.menus.Menu;
 import com.arkflame.mineclans.modernlib.menus.items.MenuItem;
+import com.arkflame.mineclans.utils.NumberUtil;
+
 import net.milkbowl.vault.economy.Economy;
 
 public class PersonalBankMenu extends Menu {
-    private final Player player;
-
     public PersonalBankMenu(Player player) {
-        super("Personal Bank", 4); // 4 rows for the menu
-        this.player = player;
-
+        super(MineClans.getInstance().getConfig().getString("menus.personal_bank.title", "Personal Bank"), 4);
+        Menu menu = this;
         // Setting the Deposit and Withdraw buttons
-        setItem(11, new MenuItem(Material.CHEST, 1, (short) 0, ChatColor.GREEN + "Deposit",
-                ChatColor.GRAY + "Click to deposit money") {
+        setItem(11, new MenuItem(Material.CHEST, 1, (short) 0, getConfigString("menus.personal_bank.deposit.name"),
+                getConfigString("menus.personal_bank.deposit.lore")) {
             @Override
             public void onClick(Player player, int slot) {
-                new DepositWithdrawMenu(player, EnteringType.DEPOSIT).openInventory(player);
+                new DepositWithdrawMenu(player, EnteringType.DEPOSIT, menu).openInventory(player);
             }
         });
 
-        setItem(15, new MenuItem(Material.HOPPER, 1, (short) 0, ChatColor.RED + "Withdraw",
-                ChatColor.GRAY + "Click to withdraw money") {
+        setItem(15, new MenuItem(Material.HOPPER, 1, (short) 0, getConfigString("menus.personal_bank.withdraw.name"),
+                getConfigString("menus.personal_bank.withdraw.lore")) {
             @Override
             public void onClick(Player player, int slot) {
-                new DepositWithdrawMenu(player, EnteringType.WITHDRAWAL).openInventory(player);
+                new DepositWithdrawMenu(player, EnteringType.WITHDRAWAL, menu).openInventory(player);
             }
         });
 
         // Set the close button
-        setItem(31, new MenuItem(Material.BARRIER, 1, (short) 0, ChatColor.RED + "Close",
-                ChatColor.GRAY + "Click to close the menu") {
+        setItem(31, new MenuItem(Material.BARRIER, 1, (short) 0, getConfigString("menus.personal_bank.close.name"),
+                getConfigString("menus.personal_bank.close.lore")) {
             @Override
             public void onClick(Player player, int slot) {
                 player.closeInventory();
@@ -48,14 +47,21 @@ public class PersonalBankMenu extends Menu {
         // Set background stained glass
         setBackground(Material.getMaterial("STAINED_GLASS_PANE"), (short) 7, " ");
     }
+
+    private String getConfigString(String path) {
+        return MineClans.getInstance().getMessages().getString(path);
+    }
 }
 
 class DepositWithdrawMenu extends Menu {
     private final Player player;
     private final EnteringType enteringType;
 
-    public DepositWithdrawMenu(Player player, EnteringType enteringType) {
-        super((enteringType == EnteringType.DEPOSIT ? "Deposit" : "Withdraw"), 4); // 4 rows menu
+    public DepositWithdrawMenu(Player player, EnteringType enteringType, Menu oldMenu) {
+        super(MineClans.getInstance().getConfig().getString(
+                enteringType == EnteringType.DEPOSIT ? "menus.deposit_withdraw.deposit.title"
+                        : "menus.deposit_withdraw.withdraw.title",
+                enteringType == EnteringType.DEPOSIT ? "Deposit" : "Withdraw"), 4); // 4 rows menu
         this.player = player;
         this.enteringType = enteringType;
 
@@ -67,11 +73,11 @@ class DepositWithdrawMenu extends Menu {
         }
 
         // Set the close button
-        setItem(31, new MenuItem(Material.BARRIER, 1, (short) 0, ChatColor.RED + "Close",
-                ChatColor.GRAY + "Click to close the menu") {
+        setItem(31, new MenuItem(Material.ARROW, 1, (short) 0, getConfigString("menus.personal_bank.close.name"),
+                getConfigString("menus.personal_bank.close.lore")) {
             @Override
             public void onClick(Player player, int slot) {
-                player.closeInventory();
+                oldMenu.openInventory(player);
             }
         });
 
@@ -80,37 +86,41 @@ class DepositWithdrawMenu extends Menu {
     }
 
     private void setDepositItems() {
-        // Add percentage buttons
-        setItem(11, new PercentageItem("Deposit 25%", Material.CHEST, 25));
-        setItem(13, new PercentageItem("Deposit 50%", Material.CHEST, 50));
-        setItem(15, new PercentageItem("Deposit 100%", Material.CHEST, 100));
+        setItem(11, new PercentageItem(enteringType, "menus.deposit_withdraw.deposit.percentages.25"));
+        setItem(13, new PercentageItem(enteringType, "menus.deposit_withdraw.deposit.percentages.50"));
+        setItem(15, new PercentageItem(enteringType, "menus.deposit_withdraw.deposit.percentages.100"));
 
         // Add specific amount button
-        setItem(29, new MenuItem(Material.PAPER, 1, (short) 0, ChatColor.YELLOW + "Specific Amount",
-                ChatColor.GRAY + "Enter specific amount to deposit") {
-            @Override
-            public void onClick(Player player, int slot) {
-                handleSpecificAmount();
-            }
-        });
+        setItem(29,
+                new MenuItem(Material.PAPER, 1, (short) 0,
+                        getConfigString("menus.deposit_withdraw.deposit.specific.name"),
+                        getConfigString("menus.deposit_withdraw.deposit.specific.lore")) {
+                    @Override
+                    public void onClick(Player player, int slot) {
+                        handleSpecificAmount();
+                    }
+                });
     }
 
     private void setWithdrawItems() {
-        setItem(13, new MenuItem(Material.HOPPER, ChatColor.RED + "Withdraw"));
-
-        // Add percentage buttons
-        setItem(10, new PercentageItem("Withdraw 25%", Material.HOPPER, 25));
-        setItem(12, new PercentageItem("Withdraw 50%", Material.HOPPER, 50));
-        setItem(14, new PercentageItem("Withdraw 100%", Material.HOPPER, 100));
+        setItem(11, new PercentageItem(enteringType, "menus.deposit_withdraw.withdraw.percentages.25"));
+        setItem(13, new PercentageItem(enteringType, "menus.deposit_withdraw.withdraw.percentages.50"));
+        setItem(15, new PercentageItem(enteringType, "menus.deposit_withdraw.withdraw.percentages.100"));
 
         // Add specific amount button
-        setItem(29, new MenuItem(Material.PAPER, 1, (short) 0, ChatColor.YELLOW + "Specific Amount",
-                ChatColor.GRAY + "Enter specific amount to withdraw") {
-            @Override
-            public void onClick(Player player, int slot) {
-                handleSpecificAmount();
-            }
-        });
+        setItem(29,
+                new MenuItem(Material.PAPER, 1, (short) 0,
+                        getConfigString("menus.deposit_withdraw.withdraw.specific.name"),
+                        getConfigString("menus.deposit_withdraw.withdraw.specific.lore")) {
+                    @Override
+                    public void onClick(Player player, int slot) {
+                        handleSpecificAmount();
+                    }
+                });
+    }
+
+    private String getConfigString(String path) {
+        return MineClans.getInstance().getMessages().getString(path);
     }
 
     private void handleSpecificAmount() {
@@ -128,12 +138,15 @@ class DepositWithdrawMenu extends Menu {
 
     // Class for percentage items
     private class PercentageItem extends MenuItem {
+        private final EnteringType enteringType;
         private final int percentage;
 
-        public PercentageItem(String name, Material material, int percentage) {
-            super(material, 1, (short) 0, ChatColor.GREEN + name,
-                    ChatColor.GRAY + "Click to deposit/withdraw " + percentage + "% of your balance");
-            this.percentage = percentage;
+        public PercentageItem(EnteringType enteringType, String path) {
+            super(enteringType == EnteringType.DEPOSIT ? Material.CHEST : Material.HOPPER, 1, (short) 0,
+                    getConfigString(path + ".name"),
+                    getConfigString(path + ".lore"));
+            this.enteringType = enteringType;
+            this.percentage = Integer.parseInt(path.substring(path.lastIndexOf('.') + 1));
         }
 
         @Override
@@ -146,17 +159,17 @@ class DepositWithdrawMenu extends Menu {
                 double balance = economy.getBalance(player);
                 double amount = balance * (percentage / 100.0);
                 MineClans.getInstance().getAPI().deposit(player, amount); // Deposit into faction's bank
-                player.sendMessage(
-                        ChatColor.GREEN + "You have deposited " + amount + " (" + percentage + "% of your balance).");
+                player.sendMessage(MineClans.getInstance().getMessages().getText("factions.deposit.success_menu")
+                        .replace("%amount%", NumberUtil.formatBalance(amount))
+                        .replace("%percentage%", String.valueOf(percentage)));
             } else {
                 double balance = faction.getBalance();
                 double amount = balance * (percentage / 100.0);
                 MineClans.getInstance().getAPI().withdraw(player, amount); // Withdraw from faction's bank
-                player.sendMessage(
-                        ChatColor.RED + "You have withdrawn " + amount + " (" + percentage + "% of your balance).");
+                player.sendMessage(MineClans.getInstance().getMessages().getText("factions.withdraw.success_menu")
+                        .replace("%amount%", NumberUtil.formatBalance(amount))
+                        .replace("%percentage%", String.valueOf(percentage)));
             }
-
-            player.closeInventory();
         }
     }
 }
